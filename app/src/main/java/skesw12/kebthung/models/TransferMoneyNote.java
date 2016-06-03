cdc12ca7a7b4342a468aa805844d3d03b137de5a
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-06-03 18:18:47 +0700

    Edit ondelete on transfernote

--- app/src/main/java/skesw12/kebthung/models/TransferMoneyNote.java
+++ app/src/main/java/skesw12/kebthung/models/TransferMoneyNote.java
@@ -5,17 +5,37 @@ package skesw12.kebthung.models;
  */
 public class TransferMoneyNote extends Note{
     private Wallet des;
-    public TransferMoneyNote(Wallet wallet,Wallet des, String purpose, double amount) {
-        super(wallet, purpose, amount);
+    private ReceiveTransferMoneyNote note;
+    private class ReceiveTransferMoneyNote extends Note{
+        private Wallet des;
+        private TransferMoneyNote note;
+        public ReceiveTransferMoneyNote(Wallet wallet,Wallet des,TransferMoneyNote note, String purpose, double amount) {
+            super(wallet, "Transfer From "+des.getName()+" : "+purpose, amount);
+            this.des = des;
+            this.note = note;
+        }
+
+        @Override
+        public void onDelete() {
+            wallet.setBalance(wallet.getBalance()-amount);
+            des.setBalance(des.getBalance()+amount);
+            des.removeNote(note);
+            wallet.removeNote(this);
+        }
     }
 
-    public String getDesName() {
-        return des.getName();
+    public TransferMoneyNote(Wallet wallet,Wallet des, String purpose, double amount) {
+        super(wallet, "Transfer To "+des.getName()+" : "+purpose, amount);
+        this.des = des;
+        note = new ReceiveTransferMoneyNote(des,wallet,this,purpose,amount);
+        des.addNote(note);
     }
 
     @Override
     public void onDelete() {
         wallet.setBalance(wallet.getBalance()+amount);
         des.setBalance(des.getBalance()-amount);
+        des.removeNote(note);
+        wallet.removeNote(this);
     }
 }
--- app/src/main/java/skesw12/kebthung/models/Wallet.java
+++ app/src/main/java/skesw12/kebthung/models/Wallet.java
@@ -1,5 +1,8 @@
 package skesw12.kebthung.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by YukiReii on 2/6/2559.
  */
@@ -7,6 +10,7 @@ public class Wallet {
     private String name;
     private double maxBalance;
     private double balance;
+    private List<Note> notes;
     public Wallet(String name,double maxBalance){
         this.name = name;
         this.maxBalance = maxBalance;
@@ -16,6 +20,7 @@ public class Wallet {
         this.name = "Wallet";
         this.maxBalance = maxBalance;
         this.balance = maxBalance;
+        notes = new ArrayList<Note>();
     }
 
     public double getBalance() {
@@ -30,14 +35,16 @@ public class Wallet {
         return name;
     }
 
-    public void getMoney(double amount){
+    public void getMoney(String purpose,double amount){
         balance+=amount;
         if (balance>maxBalance) maxBalance=balance;
+        notes.add(new GetMoneyNote(this,purpose,amount));
     }
 
-    public boolean payMoney(double amount){
+    public boolean payMoney(String purpose,double amount){
         if (balance-amount<0) return false;
         balance-=amount;
+        notes.add(new PayMoneyNote(this,purpose,amount));
         return true;
     }
 
@@ -45,10 +52,11 @@ public class Wallet {
         this.name = name;
     }
 
-    public boolean transfer(Wallet des,double amount){
+    public boolean transfer(String purpose,Wallet des,double amount){
         if (balance-amount<0) return false;
         balance-=amount;
-        des.getMoney(amount);
+        des.getMoney(purpose,amount);
+        notes.add(new TransferMoneyNote(this,des,purpose,amount));
         return true;
     }
 
@@ -60,5 +68,13 @@ public class Wallet {
     public void setBalance(double balance) {
         if (balance>maxBalance) maxBalance = balance;
         this.balance = balance;
+    }
+
+    void addNote(Note note){
+        notes.add(note);
+    }
+
+    void removeNote(Note note){
+        
     }
 }
