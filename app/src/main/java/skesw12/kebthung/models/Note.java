f37bda3ebd30e2edee69638cb877c88211b26c98
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-06-03 17:36:08 +0700

    Initilize note class

--- app/src/main/java/skesw12/kebthung/models/Note.java
+++ app/src/main/java/skesw12/kebthung/models/Note.java
@@ -3,11 +3,14 @@ package skesw12.kebthung.models;
 /**
  * Created by YukiReii on 3/6/2559.
  */
-public class Note {
+public abstract class Note {
     protected Wallet wallet;
     protected String purpose;
     protected double amount;
     public Note(Wallet wallet,String purpose,double amount){
-
+        this.wallet = wallet;
+        this.purpose = purpose;
+        this.amount = amount;
     }
+    public abstract void onDelete();
 }
