4b42f22e5a6233092dabdf95029670b29f31390e
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-06-03 17:46:57 +0700

    Complete Note Class

--- app/src/main/java/skesw12/kebthung/models/Note.java
+++ app/src/main/java/skesw12/kebthung/models/Note.java
@@ -1,9 +1,13 @@
 package skesw12.kebthung.models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by YukiReii on 3/6/2559.
  */
 public abstract class Note {
+    private long timestamp;
     protected Wallet wallet;
     protected String purpose;
     protected double amount;
@@ -11,6 +15,24 @@ public abstract class Note {
         this.wallet = wallet;
         this.purpose = purpose;
         this.amount = amount;
+        this.timestamp = System.currentTimeMillis();
     }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getFormattedTime(){
+        return new SimpleDateFormat("HH:mm:ss \ndd-MM-yyyy").format(new Date(timestamp));
+    }
+
     public abstract void onDelete();
 }
