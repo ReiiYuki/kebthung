e4dabcedec2fcf6924f68dac369081d2fd3ac2ac
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-06-04 15:02:32 +0700

    Finish WishSpinner and WalletSpinner Listener

--- app/src/main/java/skesw12/kebthung/fragments/WishFragment.java
+++ app/src/main/java/skesw12/kebthung/fragments/WishFragment.java
@@ -3,9 +3,11 @@ package skesw12.kebthung.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -51,12 +53,34 @@ public class WishFragment extends Fragment {
         initializeSpinner();
         return rootView;
     }
-
+    
     private void initializeSpinner(){
         walletAdapter = new WalletSpinnerAdapter(getActivity(),R.layout.transfer_spinner_layout, User.getInstance().getWallets());
         walletSpinner.setAdapter(walletAdapter);
+        walletSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedWallet = walletAdapter.getItem(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selectedWallet = null;
+            }
+        });
         wishAdapter = new WishSpinnerAdapter(getActivity(),R.layout.transfer_spinner_layout,User.getInstance().getWishs());
         wishSpinner.setAdapter(wishAdapter);
+        wishSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedWish = wishAdapter.getItem(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selectedWish = null;
+            }
+        });
     }
     private void refresh(){
         if (selectedWallet==null){
