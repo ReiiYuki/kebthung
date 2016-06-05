6f752668c82778aa28400a59ec09cd85b1c691af
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-06-05 17:12:22 +0700

    Complete some chart

--- app/src/main/java/skesw12/kebthung/fragments/ChartFragment.java
+++ app/src/main/java/skesw12/kebthung/fragments/ChartFragment.java
@@ -16,6 +16,8 @@ import com.db.chart.view.LineChartView;
 import com.db.chart.view.animation.Animation;
 import com.db.chart.view.animation.easing.BounceEase;
 
+import java.text.DecimalFormat;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import skesw12.kebthung.R;
@@ -26,9 +28,8 @@ import skesw12.kebthung.R;
 public class ChartFragment extends Fragment {
 
     @BindView(R.id.amount_chart) LineChartView amountChart;
-    private final String[] mLabels= {"Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep"};
-    private final float[][] mValues = {{3.5f, 4.7f, 4.3f, 8f, 6.5f, 9.9f, 7f, 8.3f, 7.0f},
-            {4.5f, 2.5f, 2.5f, 9f, 4.5f, 9.5f, 5f, 8.3f, 1.8f}};
+    private final String[] mLabels= {"","Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",""};
+    private final float[]mValues = {0,100, 200, 300, 80, 50, 900, 300,0};
 
     public ChartFragment() {}
 
@@ -43,14 +44,17 @@ public class ChartFragment extends Fragment {
     }
 
     private void editChart(){
-        LineSet dataset = new LineSet(mLabels, mValues[0]);
+        LineSet dataset = new LineSet(mLabels, mValues);
         dataset.setColor(Color.WHITE)
                 .setFill(getResources().getColor(android.R.color.holo_orange_light))
                 .setDotsColor(Color.WHITE)
                 .setThickness(4)
                 .setDashed(new float[]{10f,10f})
-                .beginAt(0);
+                .beginAt(1)
+                .endAt(8);
         amountChart.addData(dataset);
+        amountChart.setStep(150);
+        amountChart.setFontSize(40);
         amountChart.setLabelsColor(Color.WHITE);
         amountChart.show();
     }
